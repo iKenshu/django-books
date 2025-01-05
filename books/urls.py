ae725726daf2702e909626a9c329e2774ac217f7
iKenshu <kenshumorales@gmail.com>
2025-01-05 10:41:14 -0500

    Changes in urls.py

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,6 @@
 This file contains URLs for books app.
 """
 
-from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .views import BookViewSet
--- django_books/urls.py
+++ django_books/urls.py
@@ -1,17 +1,5 @@
-"""django_books URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+django_books URL Configuration
 """
 
 from django.contrib import admin
@@ -33,7 +21,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("books.urls")),
+    path("api/", include("books.urls"), name="books"),
     path("api-token-auth/", obtain_auth_token),
     path(
         "swagger/",
